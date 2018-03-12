46aa5c7f9444e4667c5f6f43a434adf8ac391d70
Saranda <sarandasadiku2@gmail.com>
2018-03-12 21:14:48 +0100

    top-products done

--- src/js/main.js
+++ src/js/main.js
@@ -6,14 +6,15 @@ $(document).ready(function(){
       slidesToShow: 3,
       adaptiveHeight: true,
       variableWidth: true,
+      arrows: false,
       responsive: [
         {
           breakpoint: 768,
           settings: {
             arrows: false,
             centerMode: true,
-            centerPadding: '40px',
-            slidesToShow: 3
+            centerPadding: '0px',
+            slidesToShow: 2
           }
         },
         {
